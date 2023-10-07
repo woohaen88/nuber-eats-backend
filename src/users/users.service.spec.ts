12f4abcbd9788e49e20a2428b26061e0f5de098f
woohaen88 <woohaen88@gmail.com>
2023-10-07 20:59:06 +0900

    Test: Mocking Test

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,12 +1,43 @@
 import { Test } from '@nestjs/testing';
 import { UsersService } from './users.services';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Verification } from './entities/verification.user.entity';
+import { JwtService } from 'src/jwt/jwt.service';
+
+const mockRepository = {
+  findOne: jest.fn(),
+  save: jest.fn(),
+  create: jest.fn(),
+  findOneBy: jest.fn(),
+  delete: jest.fn(),
+};
+
+const mockJwtService = {
+  sign: jest.fn(),
+  verify: jest.fn(),
+};
 
 describe('UserService', () => {
   let service: UsersService;
 
   beforeAll(async () => {
     const module = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        {
+          provide: getRepositoryToken(User),
+          useValue: mockRepository,
+        },
+        {
+          provide: getRepositoryToken(Verification),
+          useValue: mockRepository,
+        },
+        {
+          provide: JwtService,
+          useValue: mockJwtService,
+        },
+      ],
     }).compile();
     service = module.get<UsersService>(UsersService);
   });
