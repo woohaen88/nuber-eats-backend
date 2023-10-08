30848ef73af285a5bb77846a30f4cebc92beb7d8
woohaen88 <woohaen88@gmail.com>
2023-10-08 21:34:46 +0900

    Test: E2E Login

--- test/users.e2e-spec.ts
+++ test/users.e2e-spec.ts
@@ -7,8 +7,16 @@ import * as process from 'process';
 
 // ========== constant ==========
 const GRAPHQL_ENDPOINT = '/graphql';
+const testUser = {
+  email: 'test@test.com',
+  password: 'test123!@#',
+};
+
+// ==============================
+
 describe('UserModule (e2e)', () => {
   let app: INestApplication;
+  let jwtToken: string;
 
   beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -37,7 +45,6 @@ describe('UserModule (e2e)', () => {
   });
 
   describe('createAccount', () => {
-    const EMAIL = 'test@test.com';
     it('should create account', () =>
       request(app.getHttpServer())
         .post(GRAPHQL_ENDPOINT)
@@ -45,8 +52,8 @@ describe('UserModule (e2e)', () => {
           query: `
           mutation {
             createAccount(input: {
-              email: "${EMAIL}",
-              password: "test123!@#",
+              email: "${testUser.email}",
+              password: "${testUser.password}",
               role: Owner,
             }){
               error
@@ -57,8 +64,9 @@ describe('UserModule (e2e)', () => {
         })
         .expect(200)
         .expect((res) => {
-          expect(res.body.data.createAccount.ok).toBe(true);
-          expect(res.body.data.createAccount.error).toBe(null);
+          const { ok, error } = res.body.data.createAccount;
+          expect(ok).toBe(true);
+          expect(error).toBe(null);
         }));
     it('should fail if account already exists', () =>
       request(app.getHttpServer())
@@ -67,8 +75,8 @@ describe('UserModule (e2e)', () => {
           query: `
           mutation {
             createAccount(input: {
-              email: "${EMAIL}",
-              password: "test123!@#",
+              email: "${testUser.email}",
+              password: "${testUser.password}",
               role: Owner,
             }){
               error
@@ -79,14 +87,61 @@ describe('UserModule (e2e)', () => {
         })
         .expect(200)
         .expect((res) => {
-          expect(res.body.data.createAccount.ok).toBe(false);
-          expect(res.body.data.createAccount.error).toBe(
-            'There is a user with that email already',
-          );
+          const { ok, error } = res.body.data.createAccount;
+          expect(ok).toBe(false);
+          expect(error).toBe('There is a user with that email already');
+        }));
+  });
+  describe('login', () => {
+    it('should login with correct credentials', () =>
+      request(app.getHttpServer())
+        .post(GRAPHQL_ENDPOINT)
+        .send({
+          query: `
+          mutation {
+            login(input: {
+              email: "${testUser.email}",
+              password: "${testUser.password}"
+            }){
+              ok
+              error
+              token
+            }
+          }`,
+        })
+        .expect(200)
+        .expect((res) => {
+          const { ok, error, token } = res.body.data.login;
+          expect(ok).toBe(true);
+          expect(error).toBe(null);
+          expect(token).toEqual(expect.any(String));
+          jwtToken = token;
+        }));
+    it('should not be able to login with wrong credentials', () =>
+      request(app.getHttpServer())
+        .post(GRAPHQL_ENDPOINT)
+        .send({
+          query: `
+          mutation {
+            login(input: {
+              email: "${testUser.email}",
+              password: "wrongPassword"
+            }){
+              ok
+              error
+              token
+            }
+          }`,
+        })
+        .expect(200)
+        .expect((res) => {
+          const { ok, error, token } = res.body.data.login;
+          expect(ok).toBe(false);
+          expect(error).toBe('저기여~ 다른 패스워드를 입력하면 오또케');
+          expect(token).toBe(null);
         }));
   });
   it.todo('userProfile');
-  it.todo('login');
   it.todo('me');
   it.todo('emailVerify');
   it.todo('editProfile');
