01408714d6ba8975750e29959775112485a8aa57
woohaen88 <woohaen88@gmail.com>
2023-10-08 22:52:33 +0900

    Test: E2E UserProfile

--- test/users.e2e-spec.ts
+++ test/users.e2e-spec.ts
@@ -2,8 +2,10 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from '../src/app.module';
-import { DataSource } from 'typeorm';
+import { DataSource, Repository } from 'typeorm';
 import * as process from 'process';
+import { User } from '../src/users/entities/user.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
 
 // ========== constant ==========
 const GRAPHQL_ENDPOINT = '/graphql';
@@ -16,6 +18,7 @@ const testUser = {
 
 describe('UserModule (e2e)', () => {
   let app: INestApplication;
+  let usersRepository: Repository<User>;
   let jwtToken: string;
 
   beforeAll(async () => {
@@ -24,6 +27,7 @@ describe('UserModule (e2e)', () => {
     }).compile();
 
     app = module.createNestApplication();
+    usersRepository = module.get<Repository<User>>(getRepositoryToken(User));
 
     await app.init();
   });
@@ -141,7 +145,64 @@ describe('UserModule (e2e)', () => {
           expect(token).toBe(null);
         }));
   });
-  it.todo('userProfile');
+  describe('userProfile', () => {
+    /**
+     * userProfile test 이전에 database에 접근해서 db를 가지고옴
+     * beforAll hook으로 DB에 접근해서 가지고옴
+     * 처음 테스트할때 usersRespository를 가지고옴
+     */
+    let userId: number;
+    beforeAll(async () => {
+      const [user] = await usersRepository.find();
+      userId = user.id;
+    });
+    it("should see a user's profile", () =>
+      request(app.getHttpServer())
+        .post(GRAPHQL_ENDPOINT)
+        .set('X-JWT', jwtToken)
+        .send({
+          query: `
+      {
+        userProfile(userId:${userId}){
+          error
+          ok
+          user{
+            id
+          }
+        }
+    }`,
+        })
+        .expect(200)
+        .expect((res) => {
+          const { error, ok, user } = res.body.data.userProfile;
+          expect(ok).toBe(true);
+          expect(error).toBe(null);
+          expect(user.id).toBe(userId);
+        }));
+    it('should not find a profile', () =>
+      request(app.getHttpServer())
+        .post(GRAPHQL_ENDPOINT)
+        .set('X-JWT', jwtToken)
+        .send({
+          query: `
+          {
+            userProfile(userId:333){
+              error
+              ok
+              user{
+                id
+              }
+            }
+        }`,
+        })
+        .expect(200)
+        .expect((res) => {
+          const { error, ok, user } = res.body.data.userProfile;
+          expect(ok).toBe(false);
+          expect(error).toBe('저기여 유저가 없어라!!');
+          expect(user).toBe(null);
+        }));
+  });
   it.todo('me');
   it.todo('emailVerify');
   it.todo('editProfile');
